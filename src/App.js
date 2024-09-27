c6dfacf3987e7a0520b6d4a201ceee49ad701eb0
Abhinav-Sharmaaaa <156526721+Abhinav-Sharmaaaa@users.noreply.github.com>
2024-09-27 14:26:18 +0530

    commit 2.4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './pages/about';
 
 function App() {
   return (
-    
+    <Router>
       <Routes>
         <Route path="/about" element={<About />} />
       </Routes>
+    </Router>
   );
 }
 
