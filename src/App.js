bf342d95eeec55e2227cc0ac4413906e3708d261
Abhinav-Sharmaaaa <156526721+Abhinav-Sharmaaaa@users.noreply.github.com>
2024-09-27 13:59:22 +0530

    commit 2.3

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import About from './pages/about';
 
 function App() {
   return (
-    <Router>
+    
       <Routes>
         <Route path="/about" element={<About />} />
       </Routes>
-    </Router>
   );
 }
 
